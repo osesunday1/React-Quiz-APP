5f344fa3ac6c4eb218b24a53365f4d36a88e8225
Ose <ose.sunday@gmail.com>
2024-06-26 15:27:45 -0400

    margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,10 @@ import reactAnime from '../Animation/components/reactAnime.json'
 function Header({status}) {
     return (
       <header className='app-header'>
-        {status !== 'active' ? (<Lottie animationData={reactAnime} loop={true}  style={{ width: '200px', height: '200px' }}/>) : <img src='logo512.png' alt='React logo' />}
+        {status !== 'active' ? (<Lottie animationData={reactAnime} loop={true}  style={{ width: '150px', height: '150px' }}/>) : <img src='logo512.png' alt='React logo' />}
+         
+         
+        
         <h1>React Quiz</h1>
       </header>
     );
