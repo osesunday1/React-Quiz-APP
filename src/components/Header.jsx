c6d1654096da58289e72b85ac95718b97c2d284e
Ose <ose.sunday@gmail.com>
2024-06-26 13:23:35 -0400

    asa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,6 @@ function Header({status}) {
     return (
       <header className='app-header'>
         {status !== 'active' ? (<Lottie animationData={reactAnime} loop={true}  style={{ width: '200px', height: '200px' }}/>) : <img src='logo512.png' alt='React logo' />}
-         
-         
-        
         <h1>React Quiz</h1>
       </header>
     );
